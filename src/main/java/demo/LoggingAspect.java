e2e8f3e125211db5b20cfb78ae79fa1981e35459
narojunagaraju <narojunagarajun@gmail.com>
2024-04-19 09:57:51 +0530

    initial commit

---
+++ src/main/java/demo/LoggingAspect.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* demo.ShoppingCart.checkout())")
+    public void logger() {
+        System.out.println("loggers");
+    }
+
+    /****
+     * 1st * for any return type
+     * 2nd * for any package
+     * .* for any class
+     * .* for any method
+      */
+    @After("execution(* *.*.checkout())")
+    public void afterLogger() {
+        System.out.println("After Logger");
+    }
+}
---
+++ src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+
+    public void checkout() {
+        //Logging
+        //Authentication & Authorization
+        //Sanitize the Data
+        System.out.println("Checkout method from shopping cart");
+    }
+
+}
